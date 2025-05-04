eeb29ece0e47da3efbefd3566d646f5d33e59c8a
Dian Toshev <dian.toshev@delasport.com>
2025-05-04 20:14:10 +0300

    init commit

---
+++ ice-cream-app/layouts/default.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="default-layout">
+        <header>
+        <h1>Ice Cream App</h1>
+        </header>
+        <main>
+        <slot />
+        </main>
+        <footer>
+        <p>&copy; 2023 Ice Cream App</p>
+        </footer>
+    </div>
+</template>\
+
+<script setup lang="ts">
+
+</script>
